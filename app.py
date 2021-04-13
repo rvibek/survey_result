36c831cc86b1443385971d9113d25547445ec465
rvibek <rvibek@gmail.com>
2021-04-13 09:27:33 +0200

    update from heroku

--- app.py
+++ app.py
@@ -20,10 +20,15 @@ def load_data(nrows):
     data['time_taken'] = data['end'] - data['start']
     return data
 
+@st.cache
+def load_data2():
+    data2 = pd.read_csv("data_freetext.csv")
+    return data2
+
 
 st.title('RSD Website Survey Analysis')
 st.header('Analysis of RSD website usability survey conudcted on February 2021')
-
+# st.write('/$color{#FFCC00}{your-text-here}$')
 
 data_load_state = st.text('Loading data...')
 data = load_data(1200)
@@ -31,6 +36,9 @@ data_load_state.text('Loading data... done!')
 if st.checkbox('Show raw data'):
     st.write('Sampled raw data')
     st.write(data.sample(5))
+
+data2 = load_data2()
+# st.write(data2)
 
 final_data = data[data['duplicated_contact'] == 0]
 duplicated_count = len(data) - len(final_data)
@@ -75,6 +83,7 @@ fig.update_yaxes(title_text='Respondents')
 st.write(fig)
 
 
+
 st.markdown('---')
 st.subheader('How did you find the RSD website')
 st.markdown('Most of the user either assessed RSD website thorugh Google (and other search engine) or typed the URL directly. Around 9 per cent landed on RSD website through social media like Facebook, Twitter or Instagram.')
@@ -107,6 +116,20 @@ sort_country.append('All the countries')
 sort_country = sorted(sort_country)
 coo_to_filter = st.selectbox('Select nationality to find out how they accessed RSD site', sort_country)
 coo_graph(coo_to_filter)
+
+
+
+st.markdown('---')
+st.subheader('Country of origin')
+st.write("35 per cent of the respondents were Sudanese, whereas Eritrean and South Sudanese were 16 and 14 per cent respectively.")
+fig = px.histogram(final_data, x="what's your country of origin?", histfunc="sum", template=template, color="what's your country of origin?", category_orders={"what's your country of origin?": ['Sudanese', 'Eritrean', 'South Sudanese', 'Syrian', 'Ethiopian', 'Somali', 'Iraqi', 'Yemeni', 'Other']})
+fig.update_layout(
+    legend_title="Country of Origin")
+fig.update_xaxes(title_text="Country of Origin")
+fig.update_yaxes(title_text="Respondents")
+st.write(fig)
+
+
 
 
 st.markdown('---')
@@ -219,6 +242,27 @@ visit_coo_graph(visit_coo_to_filter)
 
 
 st.markdown('---')
+st.subheader("Is there anything you would change or remove to make the help website easier to use and/or is anything missing, perhaps a topic you would like to know more about?")
+st.markdown('')
+piedata = data2[data2.question == "Is there anything you would change or remove to make the help website easier to use and/or is anything missing, perhaps a topic you would like to know more about?"]
+fig = px.pie(piedata, names='response', values='count', template=template)
+st.write(fig)
+st.markdown("Answering to **specific suggestions related to RSD**, respondants suggested following (listed according to popularity of the reply/topic.")
+st.markdown("* Add the RSD result of First Instance and Appeal")
+st.markdown("* Add a live chat box for applicants to be able to communicate with staff / add an interactive section like Facebook comments to the website")
+st.markdown("* Update the RSD website on a regular basis and include most recent information relating to refugees and asylum-seekers")
+st.markdown("* Add a complaint box/ feedback mechanism on the website")
+st.markdown("* Provide more details about the case status on the website")
+st.markdown(
+    "* Add google maps address to the website with the location of the Office")
+st.markdown(
+    "* Make other languages easier to use and accessible throughout the website")
+st.markdown("* Reflect the changed phone numbers faster on the website")
+st.markdown(
+    "* Add the method in which the RSD interview will be conducted (Signal/Phone/ Office)")
+st.markdown("* Change the font, colors and front page of the RSD website")
+
+st.markdown('---')
 st.subheader('Do you have your own smartphone')
 st.markdown('Majority of the respondents had their own smartphones - mostly Android. 90 per cent either owned smartphone or shared smartphone with their family or frineds. 10 per cent of the respondents do not have their own smartphone. ')
 def coo_smart_graph(coo):
@@ -239,19 +283,96 @@ st.markdown('---')
 st.subheader('Do you have access to the internet vs owning smartphone')
 st.markdown('60 per cent of the respondents had access to the internet and 40 per cent did not.')
 sanky_graph = final_data.groupby(['do you always have access to the internet?','do you have your own smartphone?'])['_id'].count().to_frame().reset_index()
-sanky_graph.columns=['source', 'target', 'value']
-# sanky_graph.head()
-# # label = ["ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE"]
-# source = list(sanky_graph.source)
-# target = list(sanky_graph.target)
-# value = list(sanky_graph.value)
-# # data to dict, dict to sankey
-# link = dict(source = source, target = target, value = value)
-# node = dict( pad=50, thickness=5)
-# data = go.Sankey(link = link, node=node)
-# # plot
-# fig = go.Figure(data)
+sanky_graph.columns=['Do you have access to the internet?', 'target', 'value']
+# fig = px.sunburst(sanky_graph, path=[
+#     'Have access to the internet', 'target'], values='value', width=800, height=600, template=template)
 # st.write(fig)
-fig = px.sunburst(sanky_graph, path=[
-    'source', 'target'], values='value', width=800, height=600, template=template)
+fig = px.treemap(sanky_graph, path=[px.Constant('Do you have access to the internet?'), 'Do you have access to the internet?', 'target'], values='value',
+                 color='value', hover_data=['Do you have access to the internet?'], template=template)
 st.write(fig)
+
+
+st.markdown('---')
+st.subheader(
+    "What UNHCR services would you like to be able to access digitally / online?")
+st.markdown('')
+piedata = data2[data2.question ==
+                "What UNHCR services would you like to be able to access digitally / online?"]
+fig = px.pie(piedata, names='response', values='count', template=template)
+st.write(fig)
+st.markdown("19.4 per cent of repondants who mentioned RSD further noted that they would like to access **RSD result**, **Inquire about RSD status / case status**, **RSD interview date** and **To process reopening via the website**")
+
+
+st.markdown('---')
+st.subheader('Do you use any online services')
+st.markdown('On an average only 11 to 12 per cent of the repondants used other online services from other organisations.')
+
+
+def coo_smart_graph(coo):
+    if coo == 'All the countries':
+        piegraph_data = final_data.groupby(
+            'do you use any online services from other organisations such as the government, banks, other organisations like unhcr?')['_id'].count()
+        fig = px.pie(piegraph_data, values=piegraph_data.values,
+                     names=piegraph_data.index, template=template, color=piegraph_data.index,  color_discrete_map={'Yes': 'royalblue', "No": '#d62827'})
+        st.write(fig)
+      
+    else:
+        piegraph_data = final_data[final_data["what\'s your country of origin?"] == coo].groupby(
+            'do you use any online services from other organisations such as the government, banks, other organisations like unhcr?')['_id'].count()
+        fig = px.pie(piegraph_data, values=piegraph_data.values,
+                     names=piegraph_data.index, template=template, color=piegraph_data.index, color_discrete_map={'Yes': 'royalblue', 'No': '#d62827'})
+        st.write(fig)
+
+
+sort_online_coo = sort_country
+online_coo_to_filter = st.selectbox(
+    'Select nationality to find out whether they access other online services', sort_online_coo)
+coo_smart_graph(online_coo_to_filter)
+
+
+
+
+st.subheader("How do you feel about sharing your personal information such as your name, biometric data or other identity documents and details to help identify you for access to UNHCR's services online?")
+bardata = data2[data2.question == "How do you feel about sharing your personal information such as your name, biometric data or other identity documents and details to help identify you for access to UNHCR's services online?"]
+# st.write(bardata)
+fig = px.bar(bardata, x="response", y="count", template=template)
+fig.update_xaxes(title_text="Response")
+fig.update_yaxes(title_text="Respondants")
+st.write(fig)
+st.write("Most of respondants said they were fine sharing personal information. The who raised their concern noted **Data is not safe on the internet**,**Data will be shared with a third party provider without the Applicant's consent** and **Data will be shared with the government (of Egypt and/or of the POCs' government)**")
+
+
+st.subheader("Do you find these services easy to use? Please explain.")
+bardata = data2[data2.question ==
+                "Do you find these services easy to use? Please explain. "]
+# st.write(bardata)
+fig = px.bar(bardata, x="response", y="count", template=template)
+fig.update_xaxes(title_text="Response")
+fig.update_yaxes(title_text="Respondants")
+st.write(fig)
+st.markdown("Respondants who answered No also added **Lack of education including inability to read/ write and/or use technology hinders access**, **Not easy for older individuals with less technological awareness**, **No access to internet/smart phone **, **Afraid of being hacked / internet usage for personal information ** and **There is a language barrier** as their pressing concerns")
+
+st.markdown("---")
+st.subheader("Can we contact you again?")
+
+
+def coo_smart_graph(coo):
+    if coo == 'All the countries':
+        piegraph_data = final_data.groupby(
+            'can we contact you again in future to help us improve our digital services?')['_id'].count()
+        fig = px.pie(piegraph_data, values=piegraph_data.values,
+                     names=piegraph_data.index, template=template, color=piegraph_data.index,  color_discrete_map={'Yes': 'royalblue', "No": '#d62827'})
+        st.write(fig)
+
+    else:
+        piegraph_data = final_data[final_data["what\'s your country of origin?"] == coo].groupby(
+            'can we contact you again in future to help us improve our digital services?')['_id'].count()
+        fig = px.pie(piegraph_data, values=piegraph_data.values,
+                     names=piegraph_data.index, template=template, color=piegraph_data.index, color_discrete_map={'Yes': 'royalblue', 'No': '#d62827'})
+        st.write(fig)
+
+
+sort_online_coo = sort_country
+online_coo_to_filter = st.selectbox(
+    'Select nationality to find out whether they would like get contacted in the future', sort_online_coo)
+coo_smart_graph(online_coo_to_filter)
